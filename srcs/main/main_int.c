bfa9ca89b413ac9036c02fc650949f787745b1e2
Sophie <socarboni@gmail.com>
2021-10-29 14:29:11 +0200

    norme

--- srcs/main/main_int.c
+++ srcs/main/main_int.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 18:54:29 by scarboni          #+#    #+#             */
-/*   Updated: 2021/10/27 22:18:44 by scarboni         ###   ########.fr       */
+/*   Updated: 2021/10/29 11:57:25 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,35 @@ void	exit_error(int err)
 	exit(-err);
 }
 
+void	get_cmd_path_int_clean(char **possibles_paths, char **pures_paths,
+char *cmd_end_path)
+{
+	if (possibles_paths)
+		free_array(possibles_paths);
+	if (pures_paths)
+		free_array(pures_paths);
+	free(cmd_end_path);
+}
+
+char	*get_cmd_path_int(char *cmd_end_path, char **possibles_paths)
+{
+	char	*cmd_path;
+	int		i;
+
+	i = 0;
+	cmd_path = NULL;
+	while (possibles_paths[i])
+	{
+		if (cmd_path)
+			free(cmd_path);
+		cmd_path = ft_strjoin(possibles_paths[i], cmd_end_path);
+		if (access(cmd_path, F_OK) == 0)
+			return (cmd_path);
+		i++;
+	}
+	return (cmd_path);
+}
+
 char	*get_cmd_path(char *cmd_name, char **envp)
 {
 	char	*cmd_path;
@@ -27,26 +56,12 @@ char	*get_cmd_path(char *cmd_name, char **envp)
 	int		i;
 
 	i = 0;
-	cmd_path = NULL;
 	cmd_end_path = ft_strjoin("/", cmd_name);
 	while (strncmp("PATH", envp[i], 4) != 0)
 		i++;
 	pures_paths = ft_split(envp[i], '=');
 	possibles_paths = ft_split(pures_paths[1], ':');
-	i = 0;
-	while (possibles_paths[i])
-	{
-		if (cmd_path)
-			free(cmd_path);
-		cmd_path = ft_strjoin(possibles_paths[i], cmd_end_path);
-		if (access(cmd_path, F_OK) == 0)
-			break ;
-		i++;
-	}
-	if (possibles_paths)
-		free_array(possibles_paths);
-	if (pures_paths)
-		free_array(pures_paths);
-	free(cmd_end_path);
+	cmd_path = get_cmd_path_int(cmd_end_path, possibles_paths);
+	get_cmd_path_int_clean(possibles_paths, pures_paths, cmd_end_path);
 	return (cmd_path);
 }
